274529b9b49dee1086a92546f9b6373a35bc241e
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-02-04 03:35:51 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using BlackCoderInvite4PartyApp.Models;
+
+namespace BlackCoderInvite4PartyApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult Invite4Party()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Invite4Party(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlackCoderInvite4PartyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
